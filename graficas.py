2a07b661b0f0e6098c25daf4eaa826ba4bbeac04
samf8179 <sfb2332@gmail.com>
2018-05-19 09:42:53 -0500

    codigos

--- graficas.py
+++ graficas.py
@@ -2,6 +2,6 @@ import matplotlib.pyplot as plt
 
 x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 y=[10,20,30,40,45,46,47,48,49,55,70]
-plt.figure(num=1, figsize=(14,6))
+plt.figure(num=1, figsize=(15,6))
 plt.plot(x,y)
 plt.show()
